d90638fc01bbcb98c440c595358119d00a09bc86
Mako1688 <128003121+Mako1688@users.noreply.github.com>
2025-03-12 13:33:57 -0700

    Updated Heaurristic descriptions

--- src/heuristics.py
+++ src/heuristics.py
@@ -9,28 +9,32 @@ def evaluate_fitness(build):
 
 def four_wall_validation(build):
     # Check if the build has four walls that are similar but not necessarily symmetrical
-    # Return a score based on how well the walls meet the criteria
+    # Return a score based on how many walls the build seems to have 
+    # Make sure not too many large gaps
     score = 0
     # Implement the logic here
     return score
 
 def roofline_validation(build):
     # Check if the build has a defined roofline
-    # Return a score based on the roofline quality
+    # Return a score based on The structure of the roof 
+    # Preferred to have a sloped or tapered design
     score = 0
     # Implement the logic here
     return score
 
 def thematic_consistency(build):
     # Check if the build is thematically consistent
-    # Return a score based on thematic consistency
+    # Return a score based on thematic consistency 
+    # Use the appropriate blocks with the correct indication of key words in the build description
     score = 0
     # Implement the logic here
     return score
 
 def interior_validation(build):
     # Check if the interior is mostly hollow
-    # Return a score based on the interior quality
+    # Return a score based on the interior quality 
+    # Make sure it isnt too sparse and contains usefull blocks like furnaces, crafting tables and other usable blocks
     score = 0
     # Implement the logic here
     return score
@@ -38,6 +42,8 @@ def interior_validation(build):
 def entrance_validation(build):
     # Check if there is one open entrance 1x1x2 in the center of a wall
     # Return a score based on the entrance quality
+    # Make sure there is only one entrance and it is in the center of a wall 
+    # If even build size then prefer 2x1x2 entrance
     score = 0
     # Implement the logic here
     return score
